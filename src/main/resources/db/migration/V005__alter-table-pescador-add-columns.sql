1e531dd2145eb4bd91bd8da946dae4f079c48975
vitormedeiros911 <59974579+vitormedeiros911@users.noreply.github.com>
2020-07-14 09:44:32 -0300

    Atualiza migration 5

--- src/main/resources/db/migration/V005__alter-table-pescador-add-columns.sql
+++ src/main/resources/db/migration/V005__alter-table-pescador-add-columns.sql
@@ -1,2 +1,2 @@
-alter table add constraint fk_pescador_associacao foreign key (associacao) references associacao (codigo);
-alter table add constraint fk_pescador_porto foreign key (porto) references porto (codigo)
+alter table "pescador" add column associacao character not null;
+alter table "pescador" add column porto character not null;
