bcff307c5a2ff053044111817fa2348df3e86a91
vitormedeiros911 <59974579+vitormedeiros911@users.noreply.github.com>
2020-07-16 09:24:39 -0300

    Adicina máscara de telefone e aumenta quantidade de caracteres em cpf_cnpj nas migrations

--- src/main/resources/db/migration/V002__create-table-pessoa.sql
+++ src/main/resources/db/migration/V002__create-table-pessoa.sql
@@ -2,8 +2,8 @@ CREATE TABLE pessoa (
 	codigo serial not null primary key,
 	nome character varying(200) not null,
   data_nascimento timestamp not null,
-	cpf_cnpj character varying(15) not null,
-  rg_ie character varying(15) not null,
+	cpf_cnpj character varying(20) not null,
+  rg_ie character varying(20) not null,
   foto character varying(100) not null,
 
 	logradouro character varying(255),
