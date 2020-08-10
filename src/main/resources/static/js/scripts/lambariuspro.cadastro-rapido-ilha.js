a9d0efd85be23a7d180799d778ac91665ea573e0
vitormedeiros911 <59974579+vitormedeiros911@users.noreply.github.com>
2020-08-10 15:49:25 -0300

    Atualiza cadastro rápido de ilha

--- src/main/resources/static/js/scripts/lambariuspro.cadastro-rapido-ilha.js
+++ src/main/resources/static/js/scripts/lambariuspro.cadastro-rapido-ilha.js
@@ -1,33 +1,35 @@
 LambariusPro = LambariusPro || {};
 
 LambariusPro.IlhaCadastroRapido = (function() {
-	
-	function IlhaCadastroRapido() {
-		this.modal = $('#modal-lg');
-		this.botaoSalvar = this.modal.find('.js-modal-ilha-salvar-btn');
-		this.form = this.modal.find('form');
-		this.url = this.form.attr('action');
-		this.inputNomeIlha = $('#nomeIlha');
-		this.containerMensagemErro = $('.js-mensagem-cadastro-rapido-ilha');
-	}
-	
-	IlhaCadastroRapido.prototype.iniciar = function() {
-		this.form.on('submit', function(event) { event.preventDefault() });
-		this.modal.on('shown.bs.modal', onModalShow.bind(this));
-		this.modal.on('hide.bs.modal', onModalClose.bind(this))
-		this.botaoSalvar.on('click', onBotaoSalvarClick.bind(this));
-	}
-	
+
+  class IlhaCadastroRapido {
+    constructor() {
+      this.modal = $('#modal-lg');
+      this.botaoSalvar = this.modal.find('.js-modal-ilha-salvar-btn');
+      this.form = this.modal.find('form');
+      this.url = this.form.attr('action');
+      this.inputNomeIlha = $('#nomeIlha');
+      this.containerMensagemErro = $('.js-mensagem-cadastro-rapido-ilha');
+    }
+    iniciar() {
+      this.form.on('submit', function (event) { event.preventDefault(); });
+      this.modal.on('shown.bs.modal', onModalShow.bind(this));
+      this.modal.on('hide.bs.modal', onModalClose.bind(this));
+      this.botaoSalvar.on('click', onBotaoSalvarClick.bind(this));
+    }
+  }
+
+
 	function onModalShow() {
 		this.inputNomeIlha.focus();
 	}
-	
+
 	function onModalClose() {
 		this.inputNomeIlha.val('');
-		this.containerMensagemErro.addClass('hidden');
+		this.containerMensagemErro.addClass('d-none');
 		this.form.find('.form-group').removeClass('is-invalid');
 	}
-	
+
 	function onBotaoSalvarClick() {
 		var nomeIlha = this.inputNomeIlha.val().trim();
 		$.ajax({
@@ -39,23 +41,24 @@ LambariusPro.IlhaCadastroRapido = (function() {
 			success: onIlhaSalvo.bind(this)
 		});
 	}
-	
+
 	function onErroSalvandoIlha(obj) {
 		var mensagemErro = obj.responseText;
-		this.containerMensagemErro.removeClass('hidden');
+		this.containerMensagemErro.removeClass('d-none');
 		this.containerMensagemErro.html('<span>' + mensagemErro + '</span>');
 		this.form.find('.form-group').addClass('is-invalid');
 	}
-	
+
 	function onIlhaSalvo(ilha) {
 		var comboIlha = $('#ilha');
 		comboIlha.append('<option value=' + ilha.codigo + '>' + ilha.nome + '</option>');
 		comboIlha.val(ilha.codigo);
-		this.modal.modal('hide');
+    this.modal.modal('hide');
+    toastr.success("Ilha cadastrada com sucesso")
 	}
-	
+
 	return IlhaCadastroRapido;
-	
+
 }());
 
 $(function() {
