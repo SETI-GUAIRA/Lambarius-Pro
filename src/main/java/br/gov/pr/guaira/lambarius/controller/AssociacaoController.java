e5b3a7d3d171ee7aef3a8c342a6ea6468049f61b
Benjamin <33348261+benjaminfernandes@users.noreply.github.com>
2020-08-10 16:53:11 -0300

    Busca customizada associações

--- src/main/java/br/gov/pr/guaira/lambarius/controller/AssociacaoController.java
+++ src/main/java/br/gov/pr/guaira/lambarius/controller/AssociacaoController.java
@@ -1,16 +1,16 @@
 package br.gov.pr.guaira.lambarius.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import br.gov.pr.guaira.lambarius.domain.Associacao;
-import br.gov.pr.guaira.lambarius.exception.AssociacaoExistentException;
-import br.gov.pr.guaira.lambarius.exception.ImpossivelExcluirEntidadeException;
-import br.gov.pr.guaira.lambarius.service.AssociacaoService;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +18,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.gov.pr.guaira.lambarius.controller.page.PageWrapper;
+import br.gov.pr.guaira.lambarius.domain.Associacao;
+import br.gov.pr.guaira.lambarius.exception.AssociacaoExistentException;
+import br.gov.pr.guaira.lambarius.exception.ImpossivelExcluirEntidadeException;
+import br.gov.pr.guaira.lambarius.repository.AssociacaoRepository;
+import br.gov.pr.guaira.lambarius.repository.filter.AssociacaoFilter;
+import br.gov.pr.guaira.lambarius.service.AssociacaoService;
+
 @Controller
 @RequestMapping("/associacoes")
 public class AssociacaoController {
 
   @Autowired
   private AssociacaoService associacaoService;
+  @Autowired
+  private AssociacaoRepository associacaoRepository;
 
   @GetMapping("/novo")
   public ModelAndView novo(Associacao associacao) {
@@ -32,7 +42,7 @@ public class AssociacaoController {
     return mv;
   }
 
-  @PostMapping("/salvar")
+  @PostMapping(value = {"/novo", "/codigo"})
   public String salvar(@Valid Associacao associacao, BindingResult result, RedirectAttributes attr) {
     try {
 
@@ -52,32 +62,32 @@ public class AssociacaoController {
       attr.addFlashAttribute("error", exception.getMessage());
       return "redirect:/associacoes/novo";
     }
-
   }
 
   @GetMapping
-  public String listar(ModelMap model) {
-    model.addAttribute("associacoes", associacaoService.buscarTodos());
-    return "layout/pages/Associacoes/AssociacaoLista";
-  }
+	public ModelAndView pesquisar(AssociacaoFilter filter, @PageableDefault(size = 10) Pageable pageable, HttpServletRequest httpServletRequest) {
+		ModelAndView mv = new ModelAndView("layout/pages/Associacoes/AssociacaoLista");
+		
+		PageWrapper<Associacao> paginaWrapper = new PageWrapper<>(this.associacaoRepository.filtrar(filter, pageable),
+				httpServletRequest);
+		mv.addObject("pagina", paginaWrapper);
+		return mv;
+	}
 
-  @GetMapping("/editar/{codigo}")
+  @GetMapping("/{codigo}")
   public String editar(@PathVariable("codigo") Long codigo, ModelMap model) {
     model.addAttribute("associacao", associacaoService.buscarUm(codigo));
     return "/layout/pages/Associacoes/AssociacaoCadastro";
   }
-
-  @PostMapping("/excluir/{codigo}")
-  public String excluir(@PathVariable("codigo") Long codigo, RedirectAttributes attr) {
-    try {
-      associacaoService.excluir(codigo);
-      attr.addFlashAttribute("success", "Associação excluída com sucesso.");
-
-      return "redirect:/associacoes";
-
-    } catch (ImpossivelExcluirEntidadeException exception) {
-      attr.addFlashAttribute("error", exception.getMessage());
-      return "redirect:/associacoes";
-    }
+  
+  @DeleteMapping("/{codigo}")
+  public ResponseEntity<?> excluir(@PathVariable("codigo") Long codigo, RedirectAttributes attr) {
+    
+	  try {
+			this.associacaoService.excluir(codigo);	
+		}catch (ImpossivelExcluirEntidadeException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		return ResponseEntity.ok().build();
   }
 }
